7a3cbdf97b602cb5e7872b3827940a9158ae1a38
Saksham <sk3962364@gmail.com>
2026-05-23 15:05:10 +0530

    Project Initialization

---
+++ src/main/java/com/saksham/ShortIt/ShortItApplication.java
@@ -0,0 +1,13 @@
+package com.saksham.ShortIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saksham/ShortIt/ShortItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saksham.ShortIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
